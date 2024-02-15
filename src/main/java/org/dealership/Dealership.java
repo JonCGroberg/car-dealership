1aa1f1dfe83f3061d1102c99344aa86e14fdeb5d
Jonathan Groberg <jongroberg@gmail.com>
2024-02-15 18:24:20 -0500

    init commit

---
+++ src/main/java/org/dealership/Dealership.java
@@ -0,0 +1,4 @@
+package org.dealership;
+
+public class Dealership {
+}
---
+++ src/main/java/org/dealership/Main.java
@@ -0,0 +1,7 @@
+package org.dealership;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
